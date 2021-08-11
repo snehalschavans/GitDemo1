eb34174e6bbe224b215adbaa747c779d5a98e2da
admin <admin@DESKTOP-VKE87GT>
2021-08-11 05:52:25 +0530

    This is fresh changes

---
+++ src/StringReverse.java
@@ -0,0 +1,26 @@
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		String org_s="snehal";
+		String org_str=new String("Madam");
+		String temp_str="";
+		for(int i=org_str.length()-1;i>=0;i--)
+		{
+			temp_str=temp_str+org_str.charAt(i);
+		}
+		if(org_str.equalsIgnoreCase(temp_str))
+		{
+			System.out.println("String is palindrom!!!");
+		}
+		else
+		{
+			System.out.println("String is not Palindrom!!!");
+		}
+		
+
+	}
+
+	}
+
+
